f5c99cb4626fb9d1094c34ef155c79b6f26a9150
Popcorn RP - PrinceAlbert <85725579+alberttheprince@users.noreply.github.com>
2026-05-22 23:52:28 -0400

    initial commit

---
+++ MFH_bennys_V1/fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Agea59_Link59 - https://mxaizen-map.tebex.io/ + Edits/fixes by FalseHopeDesigns https://falsehopedesigns.tebex.io/'
+description 'Benny’s lowrider Edition - https://forum.cfx.re/t/free-mlo-bennys-lowrider-edition'
+version '1.0.0'
+
+data_file 'DLC_ITYP_REQUEST' 'stream/lr_supermod_garage_int.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/portebennysking_door_01.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/gtalife_carlift.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/ane.ytyp'
+
+this_is_a_map 'yes'
